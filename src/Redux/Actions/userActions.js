4b06de8dd7d24a2449498f376c19bd0e85898bd9
Sunnatullox <sunnatullox83@gmail.com>
2023-03-17 03:57:03 +0500

    shoes-shop deployment

--- src/Redux/Actions/userActions.js
+++ src/Redux/Actions/userActions.js
@@ -29,7 +29,7 @@ export const login = (email, password) => async (dispatch) => {
     };
 
     const { data } = await axios.post(
-      `https://shoes-server-production.up.railway.app/api/users/login`,
+      `/api/users/login`,
       { email, password },
       config
     );
@@ -67,7 +67,7 @@ export const register = (name, email, password) => async (dispatch) => {
     };
 
     const { data } = await axios.post(
-      `https://shoes-server-production.up.railway.app/api/users`,
+      `/api/users`,
       { name, email, password },
       config
     );
@@ -100,7 +100,7 @@ export const getUserDetails = (id) => async (dispatch, getState) => {
       },
     };
 
-    const { data } = await axios.get(`https://shoes-server-production.up.railway.app/api/users/${id}`, config);
+    const { data } = await axios.get(`/api/users/${id}`, config);
     dispatch({ type: USER_DETAILS_SUCCESS, payload: data });
   } catch (error) {
     const message =
@@ -133,7 +133,7 @@ export const updateUserProfile = (user) => async (dispatch, getState) => {
       },
     };
 
-    const { data } = await axios.put(`https://shoes-server-production.up.railway.app/api/users/profile`, user, config);
+    const { data } = await axios.put(`/api/users/profile`, user, config);
     dispatch({ type: USER_UPDATE_PROFILE_SUCCESS, payload: data });
     dispatch({ type: USER_LOGIN_SUCCESS, payload: data });
 
